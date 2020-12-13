3be0d752234fd265d6530037fca267289c0ac71a
cloworm <chloe.echikson@gmail.com>
2020-12-12 22:55:36 -0500

    added ci

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Toggle from '../components/Toggle'
 import Input from '../components/Input'
