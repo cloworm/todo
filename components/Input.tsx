9e6726a1cd2d54718c7a27615786ffd0353a177b
cloworm <chloe.echikson@gmail.com>
2020-12-12 22:46:54 -0500

    created gradient checkbox

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,9 +1,12 @@
+import Checkbox from './Checkbox'
+
 const Input = () => {
   return (
     <div className="relative">
-      <span
+      <Checkbox />
+      {/* <span
         className="absolute h-6 w-6 top-2.5 left-5 border border-light_veryLightGreyBlue rounded-full cursor-pointer"
-      ></span>
+      ></span> */}
       <input
         className="w-full rounded text-light_lightGreyBlue dark:bg-dark_veryDarkDesaturatedBlue py-3 pr-4 pl-16 focus:outline-none"
         placeholder="Create a new todo.."
