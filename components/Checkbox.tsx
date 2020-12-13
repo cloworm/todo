f7e77cc40a007fd165e5b6ad383f5ba69179b6d2
cloworm <chloe.echikson@gmail.com>
2020-12-12 22:47:10 -0500

    created gradient checkbox

---
+++ components/Checkbox.tsx
@@ -0,0 +1,22 @@
+const Checkbox = () => {
+  return (
+    // <div className="bg-gradient-to-br bg-clip-content from-blue-400 to-purple-600 p-10 h-10 w-10">
+    <div className="absolute top-2.5 left-5">
+      <div className="relative">
+        <input
+          type="checkbox"
+          className="form-checkbox border rounded-full focus:outline-none h-6 w-6"
+        />
+        <img
+          className="absolute top-2 left-1.5"
+          src="/images/icon-check.svg"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Checkbox
+
+// h-6 w-6 top-2.5 left-5
+// bg-gradient-to-br from-blue-400 to-purple-600
