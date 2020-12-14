695cc5d91bcf06d68938ddda2d0780af813bb730
cloworm <chloe.echikson@gmail.com>
2020-12-13 19:12:43 -0500

    removed unnecessary ref

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,23 +1,18 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 
 interface Props {
   completed?: boolean
   rounded?: boolean
   value?: string
-  onInputChange?: (value: string) => void
-  onCheckboxChange?: (checked: boolean) => void
+  onInputChange: (value: string) => void
+  onCheckboxChange: (checked: boolean) => void
   onDelete: () => void
   readonly?: boolean
   showDelete?: boolean
 }
 
 const Input = ({ rounded, value = '', completed, onInputChange, onCheckboxChange, showDelete, onDelete, readonly }: Props) => {
-  const [text, setText] = useState(value)
-  const [checked, setChecked] = useState(completed)
-  let checkboxRef: any = React.useRef<HTMLInputElement>(null)
-
   const updateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value)
     if (typeof onInputChange === 'function') {
       onInputChange(e.target.value)
     }
@@ -34,7 +29,6 @@ const Input = ({ rounded, value = '', completed, onInputChange, onCheckboxChange
   }
 
   const manuallyUpdateChecked = (checked: boolean) => {
-    setChecked(checked)
     if (typeof onCheckboxChange === 'function') {
       onCheckboxChange(checked)
     }
@@ -43,9 +37,7 @@ const Input = ({ rounded, value = '', completed, onInputChange, onCheckboxChange
   const handleInputClick = (e: React.MouseEvent<HTMLElement>) => {
     if (!readonly) return
 
-    if (checkboxRef) {
-      manuallyUpdateChecked(!checkboxRef.current.checked)
-    }
+    manuallyUpdateChecked(!completed)
   }
 
   return (
@@ -55,9 +47,8 @@ const Input = ({ rounded, value = '', completed, onInputChange, onCheckboxChange
           <input
             type="checkbox"
             className="form-checkbox border rounded-full focus:outline-none h-6 w-6 cursor-pointer"
-            defaultChecked={checked}
+            checked={completed}
             onChange={updateChecked}
-            ref={checkboxRef}
           />
           <img
             className="absolute top-2 left-1.5 pointer-events-none"
@@ -81,7 +72,7 @@ const Input = ({ rounded, value = '', completed, onInputChange, onCheckboxChange
           ${completed ? 'line-through text-light_lightGreyBlue' : 'text-light_darkGreyBlue'}
         `}
         placeholder="Create a new todo.."
-        value={text}
+        value={value}
         onChange={updateInput}
         onClick={handleInputClick}
         readOnly={readonly}
